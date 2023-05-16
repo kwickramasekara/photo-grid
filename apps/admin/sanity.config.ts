a2baf046da98a46e07099ffdcef3be66a5be3dc2
Keith Wickramasekara <6528584+kwickramasekara@users.noreply.github.com>
2023-05-15 22:31:39 -0500

    Add icon to login form

--- apps/admin/sanity.config.ts
+++ apps/admin/sanity.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "sanity";
+import { UserIcon } from "@sanity/icons";
 import { media } from "sanity-plugin-media";
 
 export default defineConfig({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
   dataset: process.env.SANITY_STUDIO_DATASET!,
+  icon: UserIcon,
   title: "Photo Grid - Admin",
   basePath: "/admin",
   plugins: [media()],
