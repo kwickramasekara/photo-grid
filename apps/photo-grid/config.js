bff3f6fd2d4bfdf2ff6302e3662c5519493c58a5
Keith Wickramasekara <6528584+kwickramasekara@users.noreply.github.com>
2023-05-22 23:13:21 -0500

    Add 11ty

---
+++ apps/photo-grid/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "Photo Grid",
+  url: "https://example.com/",
+  language: "en",
+  description: "Photography Portfolio of John Doe.",
+  author: {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    url: "https://example.com/about-me/",
+    instagram: "john_doe",
+  },
+};
