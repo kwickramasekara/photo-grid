8911acfabc407ee1570bbe915c9caf747f69ab01
Keith Wickramasekara <6528584+kwickramasekara@users.noreply.github.com>
2023-06-03 23:39:23 -0500

    Render grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+import config from "./src/_data/config";
+
 // Using JSDoc type hints for intellisense.
 // Make sure to enable "Check JS" option in VS Code settings
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.njk"],
   theme: {
-    extend: {},
+    extend: {
+      gap: {
+        grid: config.gridGap ? config.gridGap + "rem" : "0.25rem",
+      },
+    },
   },
   plugins: [],
 };
