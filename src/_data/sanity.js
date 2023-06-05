b493aa1117bad1ccb3091e460bf2c752087f5eed
Keith Wickramasekara <6528584+kwickramasekara@users.noreply.github.com>
2023-06-04 22:46:12 -0500

    Update Sanity.js

--- src/_data/sanity.js
+++ src/_data/sanity.js
@@ -2,22 +2,25 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 const config = require("./config");
 require("dotenv").config();
 
-// API call to Sanity to retrieve all image data. Results are cached for a day.
-// If you want to purge the cache, delete the .cache/sanity folder and rerun the build.
+/**
+ * API call to Sanity to retrieve all image data. Results are cached - if you want to purge it,
+ * delete the .cache/sanity folder and rerun the build.
+ * @returns {Promise}
+ */
 module.exports = async function () {
   const projectId =
     process.env.SANITY_STUDIO_PROJECT_ID || config.admin?.sanity?.projectId;
   const dataset =
     process.env.SANITY_STUDIO_DATASET || config.admin?.sanity?.dataset;
+  const APIVersion = "v2023-06-02"; // Today's date in YYYY-MM-DD format for the latest version
   const query = `*[_type == "sanity.imageAsset"] | order(_createdAt ${config.displayOrder}) { altText, title, url, "lqip":metadata.lqip, "aspectRatio":metadata.dimensions.aspectRatio, "width":metadata.dimensions.width, "height":metadata.dimensions.height }`;
-  let url = `https://${projectId}.api.sanity.io/v2023-06-02/data/query/${dataset}?query=${encodeURIComponent(
-    query
-  )}`;
+  const encodedQuery = encodeURIComponent(query);
 
-  /* This returns a promise */
+  const url = `https://${projectId}.api.sanity.io/${APIVersion}/data/query/${dataset}?query=${encodedQuery}`;
+
   return EleventyFetch(url, {
     directory: "./.cache/data-sanity",
-    duration: "1d", // save for 1 day,
+    duration: "1d", // keep for 1 day,
     type: "json",
     removeUrlQueryParams: true,
   }).then((data) => {
