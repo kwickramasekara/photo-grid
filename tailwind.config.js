01987f42b311d579b9891ee703cc257fb5516694
Keith Wickramasekara <6528584+kwickramasekara@users.noreply.github.com>
2023-06-10 15:52:25 -0500

    Reset Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ import config from "./src/_data/config";
 // Make sure to enable "Check JS" option in VS Code settings
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./site/**/*.html"],
+  content: ["./site/**/*.njk"],
   theme: {
     extend: {
       gap: {
