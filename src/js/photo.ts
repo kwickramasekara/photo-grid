29f85055692288cc3450dd32151f45182f784c35
Keith Wickramasekara <6528584+kwickramasekara@users.noreply.github.com>
2023-10-15 18:04:19 -0500

    Replace deprecated method (#18)

--- src/js/photo.ts
+++ src/js/photo.ts
@@ -1,17 +1,20 @@
 (() => {
   document.addEventListener("keydown", function (event) {
-    if (event.code === "ArrowRight") {
+    if (event.key === "ArrowRight") {
       document.getElementById("next").click();
-    } else if (event.code === "ArrowLeft") {
+    } else if (event.key === "ArrowLeft") {
       document.getElementById("previous").click();
-    } else if (event.code === "KeyD") {
+    } else if (event.key === "KeyD") {
       document.getElementById("details").click();
-    } else if (event.code === "Escape") {
+    } else if (event.key === "Escape") {
       const dialog = document.getElementById(
         "dialog-details"
       ) as HTMLDialogElement;
 
-      if (dialog.open === false) document.getElementById("close").click();
+      if (dialog.open === false) {
+        document.getElementById("close").click();
+        event.preventDefault();
+      }
     }
   });
 })();
