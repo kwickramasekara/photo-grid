4fbcc208657ee214a2cd7b8a62adbea9de116b0c
Keith Wickramasekara <6528584+kwickramasekara@users.noreply.github.com>
2023-11-19 16:23:22 -0600

    Update icons and other configs

--- src/_data/sanity.js
+++ src/_data/sanity.js
@@ -6,7 +6,7 @@ require("dotenv").config();
  * API call to Sanity to retrieve all image data.
  * Results are cached - if you want to purge it, delete the following cache folder and rerun the build:
  * node_modules/.cache/photo-grid/data/sanity
- * You can also change cache location with the CACHE_DIR environment variable.
+ * You can also change the cache location with the CACHE_DIR environment variable.
  * @returns {Promise}
  */
 module.exports = async function () {
@@ -17,12 +17,12 @@ module.exports = async function () {
   const APIVersion = "v2023-06-02"; // Today's date in YYYY-MM-DD format for the latest version
   const query = `*[_type == "sanity.imageAsset"] | order(_createdAt ${config.displayOrder}) { altText, description, title, url, "fileName": originalFilename, "lqip":metadata.lqip, "aspectRatio":metadata.dimensions.aspectRatio, "width":metadata.dimensions.width, "height":metadata.dimensions.height, "tags": opt.media.tags[]->name.current, "exif": metadata.exif }`;
   const encodedQuery = encodeURIComponent(query);
+  const cacheDir = process.env.CACHE_DIR || "./node_modules/.cache/photo-grid";
 
   const url = `https://${projectId}.api.sanity.io/${APIVersion}/data/query/${dataset}?query=${encodedQuery}`;
 
   return EleventyFetch(url, {
-    directory:
-      process.env.CACHE_DIR || "./node_modules/.cache/photo-grid/data/sanity",
+    directory: cacheDir + "/data/sanity",
     duration: "1d", // keep for 1 day,
     type: "json",
     removeUrlQueryParams: true,
