cd3657a01f7baa425227bcca9f0d07d91696a872
Keith Wickramasekara <6528584+kwickramasekara@users.noreply.github.com>
2023-11-19 19:38:11 -0600

    Add keyboard navigation and dialog functionality to photo gallery

--- src/js/photo.ts
+++ src/js/photo.ts
@@ -1,20 +1,25 @@
 (() => {
+  const dialog = document.getElementById("dialog-details") as HTMLDialogElement;
+
   document.addEventListener("keydown", function (event) {
+    console.log(event.key);
     if (event.key === "ArrowRight") {
       document.getElementById("next").click();
     } else if (event.key === "ArrowLeft") {
       document.getElementById("previous").click();
-    } else if (event.key === "KeyD") {
-      document.getElementById("details").click();
-    } else if (event.key === "Escape") {
-      const dialog = document.getElementById(
-        "dialog-details",
-      ) as HTMLDialogElement;
-
-      if (dialog.open === false) {
-        document.getElementById("close").click();
-        event.preventDefault();
+    } else if (event.key === "d" || event.key === "D") {
+      if (dialog.open) {
+        dialog.close();
+      } else {
+        dialog.showModal();
       }
+    } else if (event.key === "Escape") {
+      if (dialog.open) {
+        dialog.close();
+      } else {
+        document.getElementById("close").click();
+      }
+      event.preventDefault();
     }
   });
 })();
