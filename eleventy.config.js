fbab04d6f87e8b99ba9dd39a7049bbfc4ccfcd72
Keith Wickramasekara <6528584+kwickramasekara@users.noreply.github.com>
2023-11-21 11:20:38 -0600

    Refactor

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,53 @@
 const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+const slugify = require("@11ty/eleventy/src/Filters/Slugify");
 const photoGridConfig = require("./src/_data/config");
 const viteConfig = require("./vite.config");
+const config = require("./photo-grid.json");
 
 module.exports = function (eleventyConfig) {
+  const extractTitleFromSanityObj = (obj) =>
+    obj.title ? obj.title : obj.fileName.split(".")[0];
+
   eleventyConfig.addPlugin(EleventyVitePlugin, {
     serverOptions: {
       port: photoGridConfig.port,
     },
     viteOptions: viteConfig,
+  });
+
+  // Eleventy's RFC822 filter does not work because it expects a special date object
+  eleventyConfig.addNunjucksFilter("toRFC822Date", function (dateString) {
+    return new Date(dateString).toUTCString();
+  });
+
+  // Returns the absolute URL of a given path
+  eleventyConfig.addNunjucksFilter("toAbsoluteURL", function (urlPart) {
+    const { siteUrl, basePathName } = config;
+    const homePageURL = basePathName ? `${siteUrl}/${basePathName}` : siteUrl;
+
+    return urlPart
+      ? urlPart.charAt(0) === "/"
+        ? `${homePageURL}${urlPart}`
+        : `${homePageURL}/${urlPart}`
+      : homePageURL;
+  });
+
+  // Returns the relative URL of a given photo object (see src/_data/sanity.js)
+  eleventyConfig.addNunjucksFilter(
+    "toRelativeURLFromPhotoObj",
+    function (sanityImageObj) {
+      const extractedTitle = extractTitleFromSanityObj(sanityImageObj);
+      const slug = slugify(extractedTitle);
+
+      return config.photoPathName
+        ? `${config.photoPathName}/${slug}`
+        : `${slug}`;
+    },
+  );
+
+  // Returns the name of a given image (see src/_data/sanity.js)
+  eleventyConfig.addFilter("extractImageTitle", function (sanityImageObj) {
+    return extractTitleFromSanityObj(sanityImageObj);
   });
 
   eleventyConfig.addPassthroughCopy("./src/js");
@@ -16,13 +56,6 @@ module.exports = function (eleventyConfig) {
   // Admin
   eleventyConfig.addPassthroughCopy("./src/admin/js");
   eleventyConfig.addPassthroughCopy("./src/admin/styles");
-
-  // Returns the name of a given image (see src/_data/sanity.js)
-  eleventyConfig.addFilter("extractImageName", function (sanityImageObj) {
-    return sanityImageObj.title
-      ? sanityImageObj.title
-      : sanityImageObj.fileName.split(".")[0];
-  });
 
   return {
     dir: {
