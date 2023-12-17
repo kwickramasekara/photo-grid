151f418a83786d6124453dd5805bc9e565914838
Keith Wickramasekara <6528584+kwickramasekara@users.noreply.github.com>
2023-12-17 14:46:38 -0600

    Improve details scroll cue (#67)

--- src/js/preview.ts
+++ src/js/preview.ts
@@ -1,5 +1,36 @@
 (() => {
   const dialog = document.getElementById("dialog-details") as HTMLDialogElement;
+  const description = document.querySelector(".description-text");
+
+  // Dialog HTML elements do not have an "open" event, so we are firing a custom event when the dialog is opened
+  const mObserver = new MutationObserver((recs) => {
+    recs.forEach(({ attributeName: attr }) => {
+      if (attr === "open") dialog.dispatchEvent(new CustomEvent("dialog-open"));
+    });
+  });
+
+  // Observe the dialog's "open" attribute
+  mObserver.observe(dialog, { attributes: true });
+
+  // Fade the description text when it overflows
+  dialog?.addEventListener("dialog-open", () => {
+    if (description && description.scrollHeight > description.clientHeight) {
+      description.classList.add("fade-bottom");
+    }
+  });
+
+  // Additional fade logic for the description text when its scrolled
+  description?.addEventListener("scroll", (event) => {
+    const el = event.target as HTMLParagraphElement;
+
+    if (el.scrollHeight - el.scrollTop === el.clientHeight) {
+      el.classList.remove("fade-bottom");
+      el.classList.add("fade-top");
+    } else {
+      el.classList.add("fade-bottom");
+      el.classList.remove("fade-top");
+    }
+  });
 
   // Keyboard shortcuts
   document.addEventListener("keydown", function (event) {
